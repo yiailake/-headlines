b72410acbac743a2d07de4cc9a3f9cc33fc26d21
kevinfeng <kfeng631@gmail.com>
2016-04-21 10:58:55 +0800

    add new code

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,12 +1,25 @@
+import feedparser
+
 from flask import Flask
 
 app = Flask(__name__)
 
+BBC_FEED = 'http://feeds.bbci.co.uk/news/rss.xml'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return '''<html>
+    <body>
+        <h1>BBC Headlines</h1>
+        <b>{0}</b><br/>
+        <i>{1}</i><br/>
+        <p>{2}</p><br/>
+    </body>
+    </html>
+    '''.format(first_article.get('title'), first_article.get('published'), first_article.get('summary'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
