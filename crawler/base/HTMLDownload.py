e058a0efe63a001e38837da55a12e6fba1c72ab5
kevin feng <kfeng631@gmail.com>
2019-04-15 18:14:48 +0800

    update crawler scripts

--- crawler/base/HTMLDownload.py
+++ crawler/base/HTMLDownload.py
@@ -5,7 +5,7 @@ class HTMLDownload(object):
         if url is None:
             return
         s = requests.Session()
-        s.headers['User-Agent'] = ''
+        s.headers['User-Agent'] = 'Mozilla / 5.0(Windows NT 10.0;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 63.0.3239.132Safari / 537.36'
         res = s.get(url)
         # 判断是否正常获取
         if res.status_code == 200:
